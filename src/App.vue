4e8a775857ecb2a743f822f7b9ecf0d946737743
Leithon English <leithonenglish@gmail.com>
2021-04-10 19:45:28 -0500

    navigation start

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,18 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
+  <NavigationBar />
+  <router-view v-slot="{ Component }">
+    <component :is="Component"></component>
+  </router-view>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
-import HelloWorld from './components/HelloWorld.vue'
+import { defineComponent } from 'vue';
+import NavigationBar from './components/NavigationBar.vue';
 
 export default defineComponent({
   name: 'App',
   components: {
-    HelloWorld
+    NavigationBar
   }
 })
 </script>
---
+++ src/pages/Home.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <div class="hero h-[890px]"></div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'Home'
+})
+</script>
+
+<style lang="scss" scoped>
+.hero {
+  background: rgba(0, 0, 0, 0) url("../assets/images/hero-background.jpg") no-repeat scroll center top / 3000px;
+}
+</style>
